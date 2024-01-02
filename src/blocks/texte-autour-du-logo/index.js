cdcce8908c3ac818d30536b936b59aabdd8718e0
Géraud Henrion <geraud.henrion@gmail.com>
2024-01-02 18:18:24 +0100

    Création du bloc "texte-autour-du-logo"

--- src/blocks/texte-autour-du-logo/index.js
+++ src/blocks/texte-autour-du-logo/index.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from "@wordpress/blocks";
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -12,21 +12,110 @@ import { registerBlockType } from '@wordpress/blocks';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './style.scss';
+import "./style.scss";
 
 /**
  * Internal dependencies
  */
-import Edit from './edit';
-import Save from './save';
-import metadata from './block.json';
+import metadata from "./block.json";
+import { useBlockProps, InnerBlocks } from "@wordpress/block-editor";
 
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType( metadata.name, {
+registerBlockType(metadata.name, {
+	attributes: {},
+	category: metadata.category,
+	title: metadata.title,
 	edit: Edit,
-	save: Save,
-} );
+	save,
+});
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {JSX.Element} Element to render.
+ */
+function Edit(props) {
+	return (
+		<div
+			{...useBlockProps({ className: "potos_texte-autour-du-logo alignwide" })}
+		>
+			<InnerBlocks
+				template={[
+					[
+						"core/columns",
+						{},
+						[
+							[
+								"core/column",
+								{
+									className: "potos_column1",
+								},
+								[
+									[
+										"core/paragraph",
+										{
+											placeholder: "Lorem ipsum...",
+										},
+									],
+								],
+							],
+							[
+								"core/column",
+								{
+									className: "potos_image",
+									lock: {
+										remove: false,
+										move: false,
+									},
+								},
+								[["core/image"]],
+							],
+							[
+								"core/column",
+								{
+									className: "potos_column2",
+								},
+								[
+									[
+										"core/paragraph",
+										{
+											placeholder: "Lorem ipsum...",
+										},
+									],
+								],
+							],
+						],
+					],
+				]}
+			/>
+		</div>
+	);
+}
+
+/**
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
+ *
+ * @return {JSX.Element} Element to render.
+ */
+function save() {
+	return (
+		<div
+			{...useBlockProps.save({
+				className: "potos_texte-autour-du-logo alignwide",
+			})}
+		>
+			<InnerBlocks.Content />
+		</div>
+	);
+}
